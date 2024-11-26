fbefc7fff6cdf953318cdeb6c2291fb271aa0e39
adithyan s kumar <adithyans1438@gmail.com>
2024-11-26 14:00:04 +0530

    module issue

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const ListRouter = require('./routes/lists')
 
 dotenv.config()
 app.use(cookieParser()); // Use cookie-parser middleware
-app.UseStaticFiles();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json({ limit: '10gb' }));
